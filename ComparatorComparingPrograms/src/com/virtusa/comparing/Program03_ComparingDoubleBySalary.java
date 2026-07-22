199b47e38383400405f4e63d686b6f95d605bd27
Rajesh <rajeshyaboo2008@gmail.com>
2026-07-22 09:45:50 +0530

    Feat : Add Program03 : Comparator.comparingDouble() by Salary

---
+++ ComparatorComparingPrograms/src/com/virtusa/comparing/Program03_ComparingDoubleBySalary.java
@@ -0,0 +1,31 @@
+package com.virtusa.comparing;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import com.virtusa.model.Employee;
+
+public class Program03_ComparingDoubleBySalary {
+
+	public static void main(String[] args) {
+
+		List<Employee> employees = new ArrayList<>();
+
+		employees.add(new Employee(103, "Rajesh", 31, 55000.50));
+		employees.add(new Employee(101, "Amit", 28, 45000.75));
+		employees.add(new Employee(105, "Deepak", 35, 70000.25));
+		employees.add(new Employee(102, "Manoj", 30, 50000.10));
+		employees.add(new Employee(104, "Vikas", 33, 60000.80));
+
+		System.out.println("Before Sorting");
+
+		employees.forEach(System.out::println);
+
+		employees.sort(Comparator.comparingDouble(Employee::getSalary));
+
+		System.out.println("After Sorting");
+
+		employees.forEach(System.out::println);
+	}
+}
