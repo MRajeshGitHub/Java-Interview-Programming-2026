0384b7db4a474c7874174d08b8b4c838bd77c519
Rajesh <rajeshyaboo2008@gmail.com>
2026-07-25 19:28:38 +0530

    Feat : Add-Program01 : Stream collect to List

---
+++ Collect_Programs/src/com/virtusa/collect/Program01_ListCollect.java
@@ -0,0 +1,39 @@
+package com.virtusa.collect;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class Program01_ListCollect {
+
+	public static void main(String[] args) {
+
+		List<String> fruits = Arrays.asList("Apple", "Banana", "Mango", "Orange");
+
+		// Print with collect
+		List<String> list = fruits.stream().collect(Collectors.toList());
+		System.out.println(list);// [Apple, Banana, Mango, Orange]
+
+		// Using Filter
+		List<String> listwithA = fruits.stream().filter(i -> i.startsWith("A")).collect(Collectors.toList());
+		System.out.println(listwithA);// [Apple]
+
+		// Using Map apply all list into Uppar-case
+
+		List<String> listwithmap = fruits.stream().map(String::toUpperCase).collect(Collectors.toList());
+		System.out.println(listwithmap);// [APPLE, BANANA, MANGO, ORANGE]
+
+		// addting element in the list
+
+		// Using java 16
+
+		List<String> newList = fruits.stream().toList();
+		System.out.println(newList);
+
+		List<String> list2 = fruits.stream().map(String::toUpperCase).toList();
+		System.out.println(list2);
+
+	}
+
+}
---
+++ Collect_Programs/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Collect_Programs {
+}
