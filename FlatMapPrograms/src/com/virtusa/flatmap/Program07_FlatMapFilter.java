2e9ac7d8a7932982e41a373d5a965a2897b00d76
Rajesh <rajeshyaboo2008@gmail.com>
2026-07-30 18:12:46 +0530

    Feat : Add-Program07 : FlatMap with Filter

---
+++ FlatMapPrograms/src/com/virtusa/flatmap/Program07_FlatMapFilter.java
@@ -0,0 +1,28 @@
+package com.virtusa.flatmap;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.virtusa.model.Employee;
+
+public class Program07_FlatMapFilter {
+
+	public static void main(String[] args) {
+
+		List<Employee> employees = Arrays.asList(
+
+				new Employee(101, "Rajesh", Arrays.asList("Java", "Spring", "Git")),
+
+				new Employee(102, "Amit", Arrays.asList("SQL", "Docker", "Java")),
+
+				new Employee(103, "Rahul", Arrays.asList("Kafka", "Spring", "AWS"))
+
+		);
+
+		List<String> list = employees.stream().flatMap(e -> e.getSkills().stream()).filter(e -> e.startsWith("J"))
+				.toList();
+		System.out.println(list);
+		
+		
+	}
+}
