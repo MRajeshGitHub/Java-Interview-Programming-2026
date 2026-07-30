d373eb37fc4da719a8934904293ca2499adcad99
Rajesh <rajeshyaboo2008@gmail.com>
2026-07-30 11:02:56 +0530

    Feat : Add-Program-15 GroupingBy with collectingAndThen

---
+++ GroupingByPrograms/src/com/virtusa/model/Program15_GroupOptional.java
@@ -0,0 +1,47 @@
+package com.virtusa.model;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class Program15_GroupOptional {
+
+	public static void main(String[] args) {
+		List<Employee> employees = Arrays.asList(
+
+				new Employee(101, "Rajesh", "IT", 50000), new Employee(102, "Amit", "HR", 65000),
+				new Employee(103, "Sunil", "IT", 45000), new Employee(104, "Neha", "HR", 70000),
+				new Employee(105, "Pooja", "Admin", 55000), new Employee(106, "Rahul", "IT", 60000)
+
+		);
+
+		Map<String, Employee> collect = employees.stream()
+
+				.collect(Collectors.groupingBy(
+
+						Employee::getDepartment,
+
+						Collectors.collectingAndThen(
+
+								Collectors.maxBy(
+
+										Comparator.comparingInt(Employee::getSalary)
+
+								),
+
+								Optional::get
+
+						)
+
+				));
+		collect.forEach((dept, emp) -> {
+			System.out.println(dept);
+			System.out.println(emp);
+			System.out.println("-----------");
+		});
+
+	}
+}
