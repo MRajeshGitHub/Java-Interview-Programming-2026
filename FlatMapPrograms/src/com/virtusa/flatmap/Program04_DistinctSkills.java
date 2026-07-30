57dd875c99bfd602aa77b48e3227264a80fc41f7
Rajesh <rajeshyaboo2008@gmail.com>
2026-07-30 14:41:34 +0530

    Feat : Add-Program04 : Distinct Skills using flatMap with Set and List

---
+++ FlatMapPrograms/src/com/virtusa/flatmap/Program04_DistinctSkills.java
@@ -0,0 +1,35 @@
+package com.virtusa.flatmap;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.virtusa.model.Employee;
+
+public class Program04_DistinctSkills {
+
+	public static void main(String[] args) {
+
+		List<Employee> employees = Arrays.asList(
+
+				new Employee(101, "Rajesh", Arrays.asList("Java", "Spring", "Git")),
+
+				new Employee(102, "Amit", Arrays.asList("Java", "SQL", "Docker")),
+
+				new Employee(103, "Rahul", Arrays.asList("Spring", "Kafka", "Git"))
+
+		);
+
+		// Using Java 16
+		List<String> dSkills = employees.stream().flatMap(e -> e.getSkills().stream()).distinct().toList();
+		System.out.println(dSkills);//[Java, Spring, Git, SQL, Docker, Kafka]
+
+		// Using java 8 and setList
+
+		Set<String> sSkills = employees.stream().flatMap(e -> e.getSkills().stream()).collect(Collectors.toSet());
+
+		System.out.println(sSkills);//[Java, Git, Docker, Kafka, Spring, SQL] order not maintained with set type
+	}
+
+}
