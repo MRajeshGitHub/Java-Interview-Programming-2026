a655b21050f7959608538748c2a204803459392f
Rajesh <rajeshyaboo2008@gmail.com>
2026-07-31 18:17:23 +0530

    Feat : Add_Program-03 Maximum Number using reduce

---
+++ ReducePrograms/src/com/virtusa/reduce/Program03_MaximumNumber.java
@@ -0,0 +1,16 @@
+package com.virtusa.reduce;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Program03_MaximumNumber {
+	public static void main(String[] args) {
+
+		List<Integer> numbers = Arrays.asList(25, 10, 45, 30, 80, 60, 67, 82, 93, 88, 99);
+
+		Integer max = numbers.stream().reduce(0, Integer::max);
+		System.out.println(max);
+		
+	}
+}
