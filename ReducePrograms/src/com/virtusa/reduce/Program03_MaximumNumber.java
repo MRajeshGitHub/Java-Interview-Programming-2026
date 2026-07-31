c1474760894178845b75b3bf16d785d0fb1f2f07
Rajesh <rajeshyaboo2008@gmail.com>
2026-07-31 19:48:18 +0530

    Feat : Add-MinValue using reduce

--- ReducePrograms/src/com/virtusa/reduce/Program03_MaximumNumber.java
+++ ReducePrograms/src/com/virtusa/reduce/Program03_MaximumNumber.java
@@ -11,6 +11,11 @@ public class Program03_MaximumNumber {
 
 		Integer max = numbers.stream().reduce(0, Integer::max);
 		System.out.println(max);
-		
+
+		// min value
+
+		Integer min = numbers.stream().reduce(Integer.MAX_VALUE, (a, b) -> a < b ? a : b);
+		System.out.println(min);
+
 	}
 }
