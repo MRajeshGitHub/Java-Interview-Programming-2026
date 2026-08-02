004b084ecebefae1ae97c8e229ed1436d602c301
Rajesh <rajeshyaboo2008@gmail.com>
2026-08-02 17:24:03 +0530

    Add-Program05-get salary sum using collecoters

---
+++ ReducePrograms/src/com/virtusa/reduce/Program05_EmployeeSalarySum.java
@@ -0,0 +1,27 @@
+package com.virtusa.reduce;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Program05_EmployeeSalarySum {
+
+	public static void main(String[] args) {
+
+		List<Employee> employees = Arrays.asList(
+
+				new Employee(101, "Rajesh", 50000),
+
+				new Employee(102, "Amit", 60000),
+
+				new Employee(103, "Rahul", 45000),
+
+				new Employee(104, "Neha", 70000)
+
+		);
+
+		//Using collecotrs
+		Double salarySum = employees.stream().collect(Collectors.summingDouble(Employee::getSalary));
+		System.out.println(salarySum);
+	}
+}
