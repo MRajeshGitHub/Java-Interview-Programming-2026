0ed4cba99ed041788b2f630141e4d6a1a83c45d3
Rajesh <rajeshyaboo2008@gmail.com>
2026-08-06 12:29:12 +0530

    Feat :Add Squer using iterator

--- StreamAPIAdvanced/src/com/virtusa/streamadvanced/Program08_StreamIterate.java
+++ StreamAPIAdvanced/src/com/virtusa/streamadvanced/Program08_StreamIterate.java
@@ -1,5 +1,6 @@
 package com.virtusa.streamadvanced;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 public class Program08_StreamIterate {
@@ -7,5 +8,17 @@ public class Program08_StreamIterate {
 	public static void main(String[] args) {
 
 		Stream.iterate(1, n -> n + 2).limit(16).forEach(System.out::println);
+
+		// even numbers
+
+		List<Integer> list = Stream.iterate(2, n -> n + 2).limit(10).toList();
+
+		System.out.println(list);
+
+		// Example-4 (Squares)
+
+		List<Integer> list2 = Stream.iterate(1, n -> n + 1).limit(5).map(n -> n * n).toList();
+
+		System.out.println(list2);
 	}
 }
