78fbb1b86ff7f4c7c394f389c8740313073e51c1
Rajesh <rajeshyaboo2008@gmail.com>
2026-08-06 13:36:04 +0530

    Feat: Add-UUID Generation

--- StreamAPIAdvanced/src/com/virtusa/streamadvanced/Program09_StreamGenerate.java
+++ StreamAPIAdvanced/src/com/virtusa/streamadvanced/Program09_StreamGenerate.java
@@ -1,6 +1,7 @@
 package com.virtusa.streamadvanced;
 
 import java.util.Random;
+import java.util.UUID;
 import java.util.stream.Stream;
 
 public class Program09_StreamGenerate {
@@ -14,5 +15,10 @@ public class Program09_StreamGenerate {
 		System.out.println("--------------------");
 		Random r = new Random();
 		Stream.generate(() -> r.nextInt(100)).limit(10).forEach(System.out::println);
+
+		// Example-4 (UUID Generation)
+		System.out.println("--------------------");
+
+		Stream.generate(() -> UUID.randomUUID()).limit(5).forEach(System.out::println);
 	}
 }
