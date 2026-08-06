20810178d32c84298d2298cf4ae1179aa1526aee
Rajesh <rajeshyaboo2008@gmail.com>
2026-08-06 10:13:50 +0530

    Feat : Add-(Map + Stream.of)

--- StreamAPIAdvanced/src/com/virtusa/streamadvanced/Program07_StreamOf.java
+++ StreamAPIAdvanced/src/com/virtusa/streamadvanced/Program07_StreamOf.java
@@ -20,5 +20,12 @@ public class Program07_StreamOf {
 		Stream<Integer> stream1 = Stream.of(10, 20, 30, 40, 50, 55, 65, 67);
 
 		stream1.filter(s -> s > 20).forEach(System.out::println);
+
+		System.out.println("====================");
+		// Example-4 (Map + Stream.of)
+
+		Stream<String> names1 = Stream.of("Rajesh", "Rahul", "Amit");
+		names1.map(String::toUpperCase).forEach(System.out::println);
+
 	}
 }
