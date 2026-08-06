c2991d222530d4c043a340eddc8b7f5546b62535
Rajesh <rajeshyaboo2008@gmail.com>
2026-08-06 10:06:46 +0530

    Feat : Add-Filters with Stream.of()

--- StreamAPIAdvanced/src/com/virtusa/streamadvanced/Program07_StreamOf.java
+++ StreamAPIAdvanced/src/com/virtusa/streamadvanced/Program07_StreamOf.java
@@ -6,7 +6,7 @@ public class Program07_StreamOf {
 
 	public static void main(String[] args) {
 
-		Stream<Integer> stream = Stream.of(10, 20, 30, 40, 50);
+		Stream<Integer> stream = Stream.of(10, 20, 30, 40, 50, 55, 65, 67);
 
 		stream.forEach(System.out::println);
 		System.out.println("====================");
@@ -14,5 +14,11 @@ public class Program07_StreamOf {
 		Stream<String> names = Stream.of("Rajesh", "Rahul", "Amit");
 
 		names.forEach(System.out::println);
+
+		System.out.println("====================");
+		// Example-3 (Filter + Stream.of)
+		Stream<Integer> stream1 = Stream.of(10, 20, 30, 40, 50, 55, 65, 67);
+
+		stream1.filter(s -> s > 20).forEach(System.out::println);
 	}
 }
