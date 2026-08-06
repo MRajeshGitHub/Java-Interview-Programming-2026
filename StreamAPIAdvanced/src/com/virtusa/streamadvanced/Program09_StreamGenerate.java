01749c38ea543e6eb02643c0ba859c9616158bbf
Rajesh <rajeshyaboo2008@gmail.com>
2026-08-06 13:26:32 +0530

    Feat : Add-(Random Numbers)

--- StreamAPIAdvanced/src/com/virtusa/streamadvanced/Program09_StreamGenerate.java
+++ StreamAPIAdvanced/src/com/virtusa/streamadvanced/Program09_StreamGenerate.java
@@ -1,5 +1,6 @@
 package com.virtusa.streamadvanced;
 
+import java.util.Random;
 import java.util.stream.Stream;
 
 public class Program09_StreamGenerate {
@@ -7,5 +8,11 @@ public class Program09_StreamGenerate {
 	public static void main(String[] args) {
 
 		Stream.generate(() -> "Java").limit(10).forEach(System.out::println);
+
+		// Example-2 (Random Numbers)
+
+		System.out.println("--------------------");
+		Random r = new Random();
+		Stream.generate(() -> r.nextInt(100)).limit(10).forEach(System.out::println);
 	}
 }
