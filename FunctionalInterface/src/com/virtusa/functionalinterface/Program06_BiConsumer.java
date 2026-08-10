ffe55cd8c5a70ea2657b4721bc469972f256d515
Rajesh <rajeshyaboo2008@gmail.com>
2026-08-10 18:53:20 +0530

    feat : add -Map.forEach return Bicomsumer

--- FunctionalInterface/src/com/virtusa/functionalinterface/Program06_BiConsumer.java
+++ FunctionalInterface/src/com/virtusa/functionalinterface/Program06_BiConsumer.java
@@ -1,5 +1,6 @@
 package com.virtusa.functionalinterface;
 
+import java.util.Map;
 import java.util.function.BiConsumer;
 
 public class Program06_BiConsumer {
@@ -9,6 +10,15 @@ public class Program06_BiConsumer {
 		BiConsumer<String, Integer> printData = (name, age) -> System.out.println(name + " : " + age);
 
 		printData.accept("Rajesh", 40);// Rajesh : 40
+		//
+
+		// Map.forEach()
+
+		Map<String, Integer> employees = Map.of("Rajesh", 60000, "Rahul", 45000, "Amit", 70000);
+
+		employees.forEach((name, sal) -> {
+			System.out.println(name + "-----" + sal);
+		});
 
 	}
 
