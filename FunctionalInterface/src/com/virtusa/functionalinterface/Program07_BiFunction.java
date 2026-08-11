56c819b0155a7b2d07cf34c6f109b4aa2aed8310
Rajesh <rajeshyaboo2008@gmail.com>
2026-08-11 10:46:43 +0530

    Feat :add-Program07 BiFunction features

--- FunctionalInterface/src/com/virtusa/functionalinterface/Program07_BiFunction.java
+++ FunctionalInterface/src/com/virtusa/functionalinterface/Program07_BiFunction.java
@@ -7,7 +7,7 @@ public class Program07_BiFunction {
 	public static void main(String[] args) {
 
 		BiFunction<Integer, Integer, Integer> sum = (a, b) -> a + b;
-		System.out.println(sum.apply(11, 12));//23
+		System.out.println(sum.apply(11, 12));// 23
 
 	}
 }
