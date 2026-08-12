c665a80d6bd1b5933222110063ce842d7b363aac
Rajesh <rajeshyaboo2008@gmail.com>
2026-08-12 18:32:21 +0530

    Feat : add Zone conversion india to usa

--- LocalDate/src/com/virtusa/dateapi/ZonedDateTimeBasic.java
+++ LocalDate/src/com/virtusa/dateapi/ZonedDateTimeBasic.java
@@ -21,6 +21,14 @@ public class ZonedDateTimeBasic {
 		// London Time
 		ZonedDateTime londonTime = ZonedDateTime.now(ZoneId.of("Europe/London"));
 		System.out.println(londonTime);
+		System.out.println("--------------------------------");
 
+		// Zone Conversion
+
+		ZonedDateTime newYorkTime1 = indiaTime.withZoneSameInstant(ZoneId.of("America/New_York"));
+
+		System.out.println("India: " + indiaTime);
+		System.out.println("--------------------------");
+		System.out.println("newYorkTime : " + newYorkTime1);
 	}
 }
