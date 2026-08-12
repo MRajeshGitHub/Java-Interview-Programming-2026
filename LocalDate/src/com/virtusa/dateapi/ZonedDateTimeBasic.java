c900c766f86a29b6926998d1c99448765e766554
Rajesh <rajeshyaboo2008@gmail.com>
2026-08-12 18:28:40 +0530

    Feat : Add-Program12 International time zone features

---
+++ LocalDate/src/com/virtusa/dateapi/ZonedDateTimeBasic.java
@@ -0,0 +1,26 @@
+package com.virtusa.dateapi;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class ZonedDateTimeBasic {
+
+	public static void main(String[] args) {
+
+		// India Time
+		ZonedDateTime indiaTime = ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
+		System.out.println(indiaTime);
+		System.out.println("--------------------------------");
+
+		// New York Time
+		ZonedDateTime newYorkTime = ZonedDateTime.now(ZoneId.of("America/New_York"));
+
+		System.out.println(newYorkTime);
+		System.out.println("--------------------------------");
+
+		// London Time
+		ZonedDateTime londonTime = ZonedDateTime.now(ZoneId.of("Europe/London"));
+		System.out.println(londonTime);
+
+	}
+}
