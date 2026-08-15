0c2a5a68b3301f9045b29637ffadb9fac1c4a38c
Rajesh <rajeshyaboo2008@gmail.com>
2026-08-15 10:45:38 +0530

    Feat: Program 19 Lowest 3 Paid Employees using java 8/16

---
+++ employee_stream_api/src/com/virtusa/stream/model3/Program19LowestThreePaidEmployees.java
@@ -0,0 +1,36 @@
+package com.virtusa.stream.model3;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class Program19LowestThreePaidEmployees {
+
+	public static void main(String[] args) {
+		// Program 19 — Find Lowest 3 Paid Employees
+
+		List<Employee> employees = Arrays.asList(
+
+				new Employee(101, "Rajesh", "IT", 40, 70000, "Hyderabad", "Male"),
+
+				new Employee(102, "Amit", "HR", 28, 45000, "Delhi", "Male"),
+
+				new Employee(103, "Rahul", "IT", 35, 85000, "Hyderabad", "Male"),
+
+				new Employee(104, "Neha", "HR", 32, 55000, "Mumbai", "Female"),
+
+				new Employee(105, "Priya", "Sales", 29, 60000, "Pune", "Female"));
+
+		List<Employee> list = employees.stream().sorted(Comparator.comparing(Employee::getSalary)).limit(3).toList();
+
+		list.forEach(System.out::println);
+		System.out.println("-------------------------------");
+
+		List<Employee> collect = employees.stream().sorted(Comparator.comparing(Employee::getSalary)).limit(3)
+				.collect(Collectors.toList());
+
+		collect.forEach(System.out::println);
+	}
+}
