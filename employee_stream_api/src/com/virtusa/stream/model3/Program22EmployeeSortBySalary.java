588ee910163d5ca3a5704bb65a2ab8a8e388fc8d
Rajesh <rajeshyaboo2008@gmail.com>
2026-08-16 12:37:51 +0530

    Feat : Program-22 salary ascending order (Low → High)

---
+++ employee_stream_api/src/com/virtusa/stream/model3/Program22EmployeeSortBySalary.java
@@ -0,0 +1,31 @@
+package com.virtusa.stream.model3;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Program22EmployeeSortBySalary {
+
+	public static void main(String[] args) {
+
+		// salary ascending order (Low → High)
+
+		List<Employee> employees = Arrays.asList(
+
+				new Employee(101, "Rajesh", "IT", 40, 70000, "Hyderabad", "Male"),
+
+				new Employee(102, "Amit", "HR", 28, 45000, "Delhi", "Male"),
+
+				new Employee(103, "Rahul", "IT", 35, 85000, "Hyderabad", "Male"),
+
+				new Employee(104, "Neha", "HR", 32, 55000, "Mumbai", "Female"),
+
+				new Employee(105, "Priya", "Sales", 29, 60000, "Pune", "Female"));
+
+		List<Employee> list = employees.stream().sorted(Comparator.comparing(Employee::getSalary))
+				.collect(Collectors.toList());
+
+		list.forEach(System.out::println);
+	}
+}
