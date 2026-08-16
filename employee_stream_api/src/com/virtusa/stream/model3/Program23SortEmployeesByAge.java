bbef7280d2be2a735ef3f235de08b60d81518e24
Rajesh <rajeshyaboo2008@gmail.com>
2026-08-16 16:57:42 +0530

    Feat : Employee age Descending order

--- employee_stream_api/src/com/virtusa/stream/model3/Program23SortEmployeesByAge.java
+++ employee_stream_api/src/com/virtusa/stream/model3/Program23SortEmployeesByAge.java
@@ -27,13 +27,29 @@ public class Program23SortEmployeesByAge {
 				.collect(Collectors.toList());
 		System.out.println("Employees Sorted By Age : ");
 
-		ageList.forEach(System.out::println);//mutable list
+		ageList.forEach(System.out::println);// mutable list
+		System.out.println("----------------------------");
 
 		// only age sort
 
-		List<Integer> list = employees.stream().map(Employee::getAge).sorted().toList();//immutable list
+		List<Integer> list = employees.stream().map(Employee::getAge).sorted().toList();// immutable list
 
 		System.out.println("only age sorting : ");
 		list.forEach(System.out::println);
+		System.out.println("----------------------------");
+
+		// Age Descending
+
+		List<Employee> ageDec = employees.stream().sorted(Comparator.comparing(Employee::getAge).reversed())
+				.collect(Collectors.toList());
+
+		System.out.println("Employee sorted Age Descending..");
+		ageDec.forEach(System.out::println);
+
+		System.out.println("----------------------------");
+
+		// only age descending
+		List<Integer> list2 = employees.stream().map(Employee::getAge).sorted(Comparator.reverseOrder()).toList();
+		list2.forEach(System.out::println);
 	}
 }
