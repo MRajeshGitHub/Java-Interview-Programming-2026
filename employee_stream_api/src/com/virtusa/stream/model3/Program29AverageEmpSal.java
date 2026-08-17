0f7a900d4b4031c6cbaa0224290d632441391775
Rajesh <rajeshyaboo2008@gmail.com>
2026-08-17 11:09:44 +0530

    feat: Program 29 Employee Average sal using streams

---
+++ employee_stream_api/src/com/virtusa/stream/model3/Program29AverageEmpSal.java
@@ -0,0 +1,30 @@
+package com.virtusa.stream.model3;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Program29AverageEmpSal {
+
+	public static void main(String[] args) {
+
+		// Average Employee Salary
+
+		List<Employee> employees = Arrays.asList(
+
+				new Employee(101, "Rajesh", "IT", 40, 70000, "Hyderabad", "Male"),
+
+				new Employee(102, "Amit", "HR", 28, 45000, "Delhi", "Male"),
+
+				new Employee(103, "Rahul", "IT", 35, 85000, "Hyderabad", "Male"),
+
+				new Employee(104, "Neha", "HR", 32, 55000, "Mumbai", "Female"),
+
+				new Employee(105, "Priya", "Sales", 29, 60000, "Pune", "Female"));
+
+		Double salAvg = employees.stream().collect(Collectors.averagingDouble(Employee::getSalary));
+
+		System.out.println(salAvg);
+	}
+
+}
