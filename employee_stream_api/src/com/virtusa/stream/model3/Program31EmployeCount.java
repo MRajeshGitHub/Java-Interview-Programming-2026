e5bdd05141f314f0750b1e9b7ea52ff0be952d13
Rajesh <rajeshyaboo2008@gmail.com>
2026-08-17 11:54:08 +0530

    feat: Program-31 Employe Count in list using stream am count().

---
+++ employee_stream_api/src/com/virtusa/stream/model3/Program31EmployeCount.java
@@ -0,0 +1,30 @@
+package com.virtusa.stream.model3;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Program31EmployeCount {
+
+	// Count Total Number of Employees
+
+	public static void main(String[] args) {
+
+		List<Employee> employees = Arrays.asList(
+
+				new Employee(101, "Rajesh", "IT", 40, 70000, "Hyderabad", "Male"),
+
+				new Employee(102, "Amit", "HR", 28, 45000, "Delhi", "Male"),
+
+				new Employee(103, "Rahul", "IT", 35, 85000, "Hyderabad", "Male"),
+
+				new Employee(104, "Neha", "HR", 32, 55000, "Mumbai", "Female"),
+
+				new Employee(105, "Priya", "Sales", 29, 60000, "Pune", "Female"));
+
+		long count = employees.stream().count();
+
+		System.out.println("Total Number of Employees: ");
+		System.out.println(count);
+	}
+
+}
