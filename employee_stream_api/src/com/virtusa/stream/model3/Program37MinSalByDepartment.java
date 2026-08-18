ef1f4ba66e50e8fbb9674fc0943c2c06f10bbe33
Rajesh <rajeshyaboo2008@gmail.com>
2026-08-18 17:55:20 +0530

    Feat :Program-37 Min salary by department

---
+++ employee_stream_api/src/com/virtusa/stream/model3/Program37MinSalByDepartment.java
@@ -0,0 +1,35 @@
+package com.virtusa.stream.model3;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class Program37MinSalByDepartment {
+
+	public static void main(String[] args) {
+
+		List<Employee> employees = Arrays.asList(
+
+				new Employee(101, "Rajesh", "IT", 40, 70000, "Hyderabad", "Male"),
+
+				new Employee(102, "Amit", "HR", 28, 45000, "Delhi", "Male"),
+
+				new Employee(103, "Rahul", "IT", 35, 85000, "Hyderabad", "Male"),
+
+				new Employee(104, "Neha", "HR", 32, 55000, "Mumbai", "Female"),
+
+				new Employee(105, "Priya", "Sales", 29, 60000, "Pune", "Female"));
+
+		Map<String, Optional<Employee>> collect = employees.stream().collect(Collectors
+				.groupingBy(Employee::getDepartment, Collectors.minBy(Comparator.comparing(Employee::getSalary))));
+
+		collect.forEach((dept, min) -> {
+			System.out.println(dept);
+			System.out.println("---------");
+			System.out.println(min);
+		}
+	}
+}
