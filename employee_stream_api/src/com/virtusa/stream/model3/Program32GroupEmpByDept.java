a2e25d41a51c6eb85d3c408be2d8186208e646d6
Rajesh <rajeshyaboo2008@gmail.com>
2026-08-18 10:00:25 +0530

    Feat : Progtram-32 Group Employee By department using stream and collectors.

---
+++ employee_stream_api/src/com/virtusa/stream/model3/Program32GroupEmpByDept.java
@@ -0,0 +1,34 @@
+package com.virtusa.stream.model3;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Program32GroupEmpByDept {
+
+	public static void main(String[] args) {
+
+		List<Employee> employees = Arrays.asList(
+
+				new Employee(101, "Rajesh", "IT", 40, 70000, "Hyderabad", "Male"),
+
+				new Employee(102, "Amit", "HR", 28, 45000, "Delhi", "Male"),
+
+				new Employee(103, "Rahul", "IT", 35, 85000, "Hyderabad", "Male"),
+
+				new Employee(104, "Neha", "HR", 32, 55000, "Mumbai", "Female"),
+
+				new Employee(105, "Priya", "Sales", 29, 60000, "Pune", "Female"));
+
+		Map<String, List<Employee>> groupByDept = employees.stream()
+				.collect(Collectors.groupingBy(Employee::getDepartment));
+
+		groupByDept.forEach((dept, list) -> {
+			System.out.println(dept);
+			System.out.println("----------");
+			list.forEach(System.out::println);
+		});
+	}
+
+}
