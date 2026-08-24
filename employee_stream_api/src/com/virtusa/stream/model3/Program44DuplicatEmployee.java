89911efa6f97ee6c5ef0867366927bd722e8324b
Rajesh <rajeshyaboo2008@gmail.com>
2026-08-24 10:55:22 +0530

    Feat : implements duplicate employee using Sort.

--- employee_stream_api/src/com/virtusa/stream/model3/Program44DuplicatEmployee.java
+++ employee_stream_api/src/com/virtusa/stream/model3/Program44DuplicatEmployee.java
@@ -1,6 +1,7 @@
 package com.virtusa.stream.model3;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -11,8 +12,7 @@ public class Program44DuplicatEmployee {
 
 	public static void main(String[] args) {
 
-		
-		//Find Duplicate Employees
+		// Find Duplicate Employees
 		List<Employee> employees = Arrays.asList(
 
 				new Employee(101, "Rajesh", "IT", 40, 70000, "Hyderabad", "Male"),
@@ -32,7 +32,18 @@ public class Program44DuplicatEmployee {
 		Map<Integer, Long> count = employees.stream()
 				.collect(Collectors.groupingBy(Employee::getId, Collectors.counting()));
 
-		count.entrySet().stream().filter(e -> e.getValue() > 1).forEach(e ->System.out.println("Duplicate ID is :"+e.getKey()));
+		count.entrySet().stream().filter(e -> e.getValue() > 1)
+				.forEach(e -> System.out.println("Duplicate ID is :" + e.getKey()));
+
+		// 2nd Approach
+		System.out.println("===========================");
+		employees.sort(Comparator.comparing(Employee::getId));
+		System.out.println("Dulicate Employee :");
+		for (int i = 0; i < employees.size() - 1; i++) {
+			if (employees.get(i).getId() == employees.get(i + 1).getId()) {
+				System.out.println(employees.get(i));
+			}
+		}
 
 	}
 
