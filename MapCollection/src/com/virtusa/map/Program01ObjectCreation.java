cca45a896dce2c37655777ee5cd84773a0bf71ae
Rajesh <rajeshyaboo2008@gmail.com>
2026-08-24 19:20:38 +0530

    Object compare beween equals() & ==.

--- MapCollection/src/com/virtusa/map/Program01ObjectCreation.java
+++ MapCollection/src/com/virtusa/map/Program01ObjectCreation.java
@@ -10,17 +10,12 @@ public class Program01ObjectCreation {
 		Employee emp1 = new Employee(1, "Rajesh", "IT", 10000.0);
 
 		System.out.println(emp == emp1);// false
+		System.out.println(emp.equals(emp1));// false
 
 		Employee emp2 = emp1;
-
-		System.out.println(emp1);// com.virtusa.map.Employee@701fc37a
-
-		System.out.println(emp2);// com.virtusa.map.Employee@701fc37a
-
 		System.out.println(emp1 == emp2);// true
+		System.out.println(emp1.equals(emp2));// true
 
-		emp2.name = "Amit";
-		System.out.println(emp1.name);
 	}
 
 }
