904676194a6f1ff5fe8cdec44c3f6c98031af7d5
Rajesh <rajeshyaboo2008@gmail.com>
2026-08-24 18:46:26 +0530

    Feat : object comparidion and changes for ==operator

--- MapCollection/src/com/virtusa/map/Program01ObjectCreation.java
+++ MapCollection/src/com/virtusa/map/Program01ObjectCreation.java
@@ -8,19 +8,19 @@ public class Program01ObjectCreation {
 
 		Employee emp = new Employee(1, "Rajesh", "IT", 10000.0);
 		Employee emp1 = new Employee(1, "Rajesh", "IT", 10000.0);
-		Employee emp2 = new Employee(2, "Vijay", "HR", 2000.0);
-		Employee emp3 = new Employee(3, "Sanjay", "HR", 3300.0);
-		emp.displayEmployee();// Employee Ordinary method
-		System.out.println("-----------------------");
-		emp1.displayEmployee();
-
-		System.out.println(emp);// com.virtusa.map.Employee@6a79c292
-
-		System.out.println(emp1);// com.virtusa.map.Employee@37574691
-		System.out.println(emp2);
-		System.out.println(emp3);
 
 		System.out.println(emp == emp1);// false
+
+		Employee emp2 = emp1;
+
+		System.out.println(emp1);// com.virtusa.map.Employee@701fc37a
+
+		System.out.println(emp2);// com.virtusa.map.Employee@701fc37a
+
+		System.out.println(emp1 == emp2);// true
+
+		emp2.name = "Amit";
+		System.out.println(emp1.name);
 	}
 
 }
