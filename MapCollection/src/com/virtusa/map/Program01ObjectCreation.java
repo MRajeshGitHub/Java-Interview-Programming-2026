82a846c2df1193980659382d72ae2bbc51f9b905
Rajesh <rajeshyaboo2008@gmail.com>
2026-08-24 18:28:34 +0530

    Feat : Same data comparsion in object creation.

--- MapCollection/src/com/virtusa/map/Program01ObjectCreation.java
+++ MapCollection/src/com/virtusa/map/Program01ObjectCreation.java
@@ -7,11 +7,20 @@ public class Program01ObjectCreation {
 		// Employee Object
 
 		Employee emp = new Employee(1, "Rajesh", "IT", 10000.0);
-		Employee emp1 = new Employee(2, "Ajay", "HR", 20000.0);
-
+		Employee emp1 = new Employee(1, "Rajesh", "IT", 10000.0);
+		Employee emp2 = new Employee(2, "Vijay", "HR", 2000.0);
+		Employee emp3 = new Employee(3, "Sanjay", "HR", 3300.0);
 		emp.displayEmployee();// Employee Ordinary method
 		System.out.println("-----------------------");
 		emp1.displayEmployee();
+
+		System.out.println(emp);// com.virtusa.map.Employee@6a79c292
+
+		System.out.println(emp1);// com.virtusa.map.Employee@37574691
+		System.out.println(emp2);
+		System.out.println(emp3);
+
+		System.out.println(emp == emp1);// false
 	}
 
 }
