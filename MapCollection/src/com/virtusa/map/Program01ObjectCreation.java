7de4215356dffecf3bebc4d1a6ba08336d73b654
Rajesh <rajeshyaboo2008@gmail.com>
2026-08-25 10:40:32 +0530

    Feat : Implements object comparision custom equal() logics

--- MapCollection/src/com/virtusa/map/Program01ObjectCreation.java
+++ MapCollection/src/com/virtusa/map/Program01ObjectCreation.java
@@ -5,15 +5,23 @@ public class Program01ObjectCreation {
 	public static void main(String[] args) {
 
 		// Employee Object
+		Employee e1 = new Employee(101, "Rahul", "IT", 50000);
+		Employee e2 = new Employee(101, "Rahul", "HR", 70000);
 
-		Employee emp1 = new Employee(101, "Rahul", "IT", 50000);
+		System.out.println(e1.equals(e2));// true
 
-		Employee emp2 = new Employee(101, "Rahul", "IT", 60000);
+		Employee e3 = new Employee(101, "Rahul", "IT", 50000);
+		Employee e4 = new Employee(102, "Rahul", "IT", 50000);
 
-		Employee emp3 = new Employee(102, "Rahul", "IT", 50000);
+		System.out.println(e3.equals(e4));// false
 
-		System.out.println(emp1.equals(emp2));
-		System.out.println(emp1.equals(emp3));
+		Employee e5 = new Employee(101, "Rahul", "IT", 50000);
+		Employee e6 = new Employee(101, "RAHUL", "IT", 50000);
+
+		System.out.println(e5.equals(e6));//false
+		
+		
+		
 
 	}
 
