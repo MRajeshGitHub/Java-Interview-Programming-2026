0ad993787c5bef7133ea281359ea011613a74a37
Rajesh <rajeshyaboo2008@gmail.com>
2026-09-23 18:32:44 +0530

    feat : Implements another logic for same program.

--- Strings/src/com/strings/CountWords.java
+++ Strings/src/com/strings/CountWords.java
@@ -6,9 +6,14 @@ public class CountWords {
 
 		String name = "java is easy to learn";
 
-		String[] words = name.split(" ");
+		// String[] words = name.split(" ");
+
+		String[] split = name.trim().split("\\s+");
 
 		System.out.println(name);
-		System.out.println("Word length is : " + words.length);
+		// System.out.println("Word length is : " + words.length);
+
+		System.out.println("word length is : " + split.length);
+
 	}
 }
