165aa316a05e4b9ee1dbf17cb38916587f019609
Rajesh <rajeshyaboo2008@gmail.com>
2026-09-23 19:34:09 +0530

    Feat : Implements LongestWord count in string.

---
+++ Strings/src/com/strings/LongestWord.java
@@ -0,0 +1,36 @@
+package com.strings;
+
+public class LongestWord {
+
+	public static void main(String[] args) {
+
+		String sentance = "java spring boot developer in hyderabad";
+		
+		
+		if(sentance==null || sentance.isBlank()) {
+			
+			System.out.println("no word found");
+		}
+		
+		else {
+
+		String[] split = sentance.split("\\s+");
+
+		String longest = "";
+
+		for (String word : split) {
+
+			if (word.length() > longest.length()) {
+				longest = word;
+			}
+		}
+		
+
+		System.out.println(sentance);
+
+		System.out.println(longest);
+
+		System.out.println(longest.length());
+		}
+	}
+}
